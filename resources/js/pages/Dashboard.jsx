bb7416bd14cecfc89821122a00bc71f142053d23
Wahyu-Harta-Sasmita <wahyusasmitaharta561@gmail.com>
2024-12-27 08:03:53 +0800

    model

--- resources/js/pages/Dashboard.jsx
+++ resources/js/pages/Dashboard.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import ValidasiData from '../components/admin/validasidata';
-import PendaftaranManual from '../components/admin/pendaftaran';
-import ManajemenData from '../components/admin/manajemen';
-import LaporanData from '../components/admin/laporan';
-import Sidebar from '../components/admin/sidebar';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import ValidasiData from '../components/admin/validasidata';
+// import PendaftaranManual from '../components/admin/pendaftaran';
+// import ManajemenData from '../components/admin/manajemen';
+// import LaporanData from '../components/admin/laporan';
+// import Sidebar from '../components/admin/sidebar';
 
 const Dashboard = () => {
   return (
-    <Router>
-      <div className="flex">
-        {/* Sidebar ditampilkan di sebelah kiri */}
-        <Sidebar />
+    // <Router>
+    //   <div className="flex">
+    //     {/* Sidebar ditampilkan di sebelah kiri */}
+    //     <Sidebar />
 
-        {/* Konten halaman ditampilkan di sebelah kanan */}
-        <div className="flex-grow p-4">
-          <Routes>
-            {/* Redirect ke /validasi jika pengguna mengakses / */}
-            <Route path="/admin" element={<ValidasiData />} />
-            <Route path="/validasi" element={<ValidasiData />} />
-            <Route path="/pendaftaran" element={<PendaftaranManual />} />
-            <Route path="/manajemen" element={<ManajemenData />} />
-            <Route path="/laporan" element={<LaporanData />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    //     {/* Konten halaman ditampilkan di sebelah kanan */}
+    //     <div className="flex-grow p-4">
+    //       <Routes>
+    //         {/* Redirect ke /validasi jika pengguna mengakses / */}
+    //         <Route path="/admin" element={<ValidasiData />} />
+    //         <Route path="/validasi" element={<ValidasiData />} />
+    //         <Route path="/pendaftaran" element={<PendaftaranManual />} />
+    //         <Route path="/manajemen" element={<ManajemenData />} />
+    //         <Route path="/laporan" element={<LaporanData />} />
+    //       </Routes>
+    //     </div>
+    //   </div>
+    // </Router>
+    <h1>Hello World</h1>
   );
 };
 
--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navbar from '../components/user/navbar'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
   )
 }
 
